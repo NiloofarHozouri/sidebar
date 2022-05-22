b2ccbf3e1f1147089474c6ba398e6b60eb31c12a
niloofar <niloofar.h.1379@gmail.com>
2022-05-22 19:58:52 +0430

    sidebar

---
+++ j.js
@@ -0,0 +1,6 @@
+const ctrlBtn = document.querySelector('.ctrlbtn');
+const sideBar = document.querySelector('.sidebar');
+ctrlBtn.addEventListener('click' , function () {
+    sideBar.classList.toggle('show');
+    ctrlBtn.classList.toggle('open');
+});
